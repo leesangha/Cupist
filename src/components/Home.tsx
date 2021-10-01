60e7b0bd4f8c11997e506ae3f3fc6527f60d5546
leesangha <sanghalee1@naver.com>
2021-10-02 05:51:01 +0900

    add components

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Glam</li>
+          <li>Live</li>
+          <li>Nearby</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default Home;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Profile: React.FC = () => {
+  return (
+    <section>
+      <div>
+        <div>picture</div>
+        <div>
+          <ul>
+            <li>
+              <label>nickname</label>
+            </li>
+            <li>sex</li>
+            <li>birth</li>
+            <li>location</li>
+            <hr />
+            <li>description</li>
+            <hr />
+            <li>height</li>
+            <li>body</li>
+            <hr />
+            <li>work</li>
+            <li>job</li>
+            <li>university</li>
+            <li>school</li>
+            <hr />
+            <li>character</li>
+            <li>religion</li>
+            <li>drink</li>
+            <li>smoke</li>
+            <li>blood</li>
+            <li>race</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Profile;
