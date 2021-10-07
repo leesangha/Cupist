7abccc2ad07a214e8d9b59b4121b3478d35644b0
leesangha <sanghalee1@naver.com>
2021-10-07 20:39:58 +0900

    asset 테스트

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Background from "../assets/human.jpg";
 import Card from "./Card";
 const Tab: React.FC = () => {
   const [toggle, setToggle] = useState<number>(1);
@@ -46,6 +47,25 @@ const Title = styled.div<{ active: boolean }>`
 `;
 const Content = styled.div<{ active: boolean }>`
   display: ${(props) => (props.active ? "block" : "none")};
+  position: relative;
+  width: 300px;
+  height: 400px;
+  background: url(${Background});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
   transition: 0.3s ease-in;
+  h1 {
+    position: absolute;
+    margin-top: 300px;
+    margin-left: 10px;
+    background: rgba(255, 255, 255, 0.15);
+    border-radius: 4px;
+    /* border: 1px solid rgba(255, 255, 255, 0.18); */
+    color: white;
+    font-family: cursive;
+    font-size: 12px;
+    font-weight: lighter;
+  }
 `;
 export default Tab;
